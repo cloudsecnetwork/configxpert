dcbb8a1debb5b2ffa36e8367b9f50ec0c4a9bd93
Yak07 <kunle.akanmode@gmail.com>
2023-08-19 00:31:59 +0100

    Users Configured With Cisco Type 7 Password Hashing Algorithm Class

---
+++ rules/cisco/access_controls/CISCO_115_UsersWithCiscoType7.py
@@ -0,0 +1,18 @@
+class UsersWithCiscoType7:
+    def __init__(self, configuration):
+        self.configuration = configuration
+
+    def check(self):
+        cisco_type7_usernames = []
+        for line_number, line in enumerate(self.configuration.splitlines(), start=1):
+            if "username" in line.lower() and "secret" in line.lower():
+                parts = line.split()
+                if len(parts) >= 4 and parts[3].startswith("7"):  # Check if the secret is using Cisco Type 7
+                    username = parts[1]
+                    cisco_type7_usernames.append(f"username with Cisco Type 7 secret: {username} on line {line_number}")
+
+        if cisco_type7_usernames:
+            comment = ", ".join(cisco_type7_usernames)
+            return {"value": "FAIL", "comment": f"Users Configured With Cisco Type 7 Password Hashing Algorithm: {comment}"}
+        else:
+            return {"value": "PASS", "comment": ""}
